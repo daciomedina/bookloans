2aa99eefb02574ce42819fb44cbc4dc39845614b
Dacio <dacio@veiss.com>
2021-12-28 13:44:38 +0100

    add js class to date type

--- src/Form/BooksLoansType.php
+++ src/Form/BooksLoansType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\BooksLoans;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType as TypeDateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,10 @@ class BooksLoansType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('logdate')
-            //->add('returned')
+            ->add('logdate', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => ['class' => 'js-datepicker'],
+            ])
             ->add('UserDNI')
             ->add('UserName')
             ->add('UserPhone')
